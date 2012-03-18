8e8a0ba4ba620eadfc52d55e04140874af0fada0
Aaron Thomson <aaronthomson@gmail.com>
2012-03-19 08:39:22 +1100

    Playing with data

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ var server = http.createServer(function(request, response) {
 	request.addListener('end', function() {
 		var location = url.parse(request.url, true),
         params = (location.query || request.headers);
-
-				console.log(location);
 		
 		var file = new static.Server('./public', {
       cache: false
@@ -31,26 +29,7 @@ var server = http.createServer(function(request, response) {
 	request.addListener('data', function(data) {
 		var location = url.parse(request.url, true);
 		if (location.pathname == '/update') {
-		  parser.parseComplete(data);
-		  
-			for(var key in handler.dom) {
-				if(key%2 == 1) {
-					//TODO: get all the incidents
-				  var suburb    = handler.dom[key].children[0].children[1].children[1].children[0].data;
-				  var street    = handler.dom[key].children[0].children[1].children[3].children[0].data;
-				  var xStreet   = handler.dom[key].children[0].children[1].children[7].children[0].data;
-				  var qualifier = handler.dom[key].children[0].children[1].children[5].children[0].data;
-
-				  var address = street.toString() + " " + qualifier.toString() + " " +xStreet.toString() + ", " + suburb.toString() + ", NSW"; // Carrington Rd at Bronte Rd, Waverly, NSW
-
-				console.log(address);
-
-				  fServer.getClient().publish('/messages', {
-				  	title: "POST"
-		      , address: address
-			    })
-				}
-			}
+		  publishTraffic(data);
 		  
 		  response.writeHead(200);
 		  response.end("Success!");
@@ -60,3 +39,52 @@ var server = http.createServer(function(request, response) {
 fServer.attach(server)
 
 server.listen(8000);
+
+var publishTraffic = function(data) {
+	
+	parser.parseComplete(data);
+	
+	console.log(Object.keys(handler.dom))
+  
+	for(var key in Object.keys(handler.dom)) {
+		
+			var element = handler.dom[key];
+			
+			console.log("-----" + element);
+			console.log("-----" + element[0]);
+			
+			if (element != undefined) {
+			
+		    var suburb    = element.children[0].children[1].children[1].children[0].data;
+		    var street    = element.children[0].children[1].children[3].children[0].data;
+		    var xStreet   = element.children[0].children[1].children[7].children[0].data;
+		    var qualifier = element.children[0].children[1].children[5].children[0].data;
+        
+		    var address = street.toString() + " " + qualifier.toString() + " " +xStreet.toString() + ", " + suburb.toString() + ", NSW"; // Carrington Rd at Bronte Rd, Waverly, NSW
+        
+		    fServer.getClient().publish('/messages', {
+		    	title: "POST"
+        , address: address
+	      })
+			  console.log("DONE")
+		  }
+	}
+};
+
+var feed = {
+  host: 'livetraffic.rta.nsw.gov.au',
+  port: 80,
+  path: '/traffic/rss/syd-metro.atom'
+};
+
+var getFeed = function() {
+	http.get(feed, function(res) {
+	  res.on('data', function (data) {
+		    publishTraffic(data);
+		  });
+	}).on('error', function(e) {
+	  console.log("Got error: " + e.message);
+	});
+};
+
+setInterval(function() { getFeed() }, 1000);
