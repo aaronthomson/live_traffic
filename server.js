9df8d8d1b5a09fec8258f213df1cbc59e57fe20d
Aaron Thomson <aaronthomson@gmail.com>
2012-03-21 12:30:54 +1100

    Works on node v0.6.13

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-require.paths.unshift(__dirname + "/vendor");
-
 var http       = require('http'),
-    static     = require('node-static/lib/node-static'),
+    static     = require('node-static'),
 		Faye       = require('faye'),
     url        = require('url'),
     htmlparser = require("htmlparser"),
@@ -13,7 +11,6 @@ var http       = require('http'),
 var jquery = fs.readFileSync("./jquery-1.7.1.min.js").toString();
 
 var handler = new htmlparser.DefaultHandler(function (error, dom) {
-    // console.log(dom);
 });
 
 var parser = new htmlparser.Parser(handler);
@@ -46,14 +43,8 @@ server.listen(8000);
 
 var publishTraffic = function(data) {
 	
-  xmlParser(data, function(error, result) {
-	if (error) {
-		console.log(error);
-		return;
-	}
-	
-	  for (var index in result.entry){		
-		
+  xmlParser(data, function(error, result) {	
+	  for (var index in result.entry){
 		  if (result.entry[index].content != undefined){
 		
 			  jsdom.env({
@@ -62,6 +53,7 @@ var publishTraffic = function(data) {
 			      jquery
 			    ],
 			    done: function(errors, window) {
+				
 			    	var street    = window.$(".tiw-rowStreetTitle:first").text().toLowerCase().replace('various roads', '');
 			  	 	var suburb    = window.$(".tiw-rowSuburbStreetTitle").text().toLowerCase().replace('various roads', '').replace('shire council area', '');
 						
@@ -72,10 +64,10 @@ var publishTraffic = function(data) {
 							address = suburb + ", NSW";
 						}
 						
-						console.log(address);
-						
 						var image = window.$(".tiw-rowCategoryIcon img").attr('src');
-						
+				
+						console.log(address);
+				
 						fServer.getClient().publish('/messages', {
 			      	address: address,
 							image: image
@@ -105,4 +97,4 @@ var getFeeds = function() {
 };
 
 setTimeout(function() { getFeeds() }, 5000);
-setInterval(function() { getFeeds() }, 30000);
+// setInterval(function() { getFeeds() }, 30000);
